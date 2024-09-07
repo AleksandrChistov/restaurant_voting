3f46e0b3efebcf9cc89a9324f80aea0f420419cf
Александр Чистов <aleksandrchistov.ru@yandex.ru>
2024-09-07 17:51:22 +0700

    Add roles auth

--- src/main/java/ru/aleksandrchistov/restaurantvoting/config/WebSecurityConfig.java
+++ src/main/java/ru/aleksandrchistov/restaurantvoting/config/WebSecurityConfig.java
@@ -3,17 +3,23 @@ package ru.aleksandrchistov.restaurantvoting.config;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.aleksandrchistov.restaurantvoting.AuthUser;
+import ru.aleksandrchistov.restaurantvoting.model.Role;
 import ru.aleksandrchistov.restaurantvoting.model.User;
 import ru.aleksandrchistov.restaurantvoting.repository.UserRepository;
 
 import java.util.Optional;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -36,5 +42,16 @@ public class WebSecurityConfig {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService())
                 .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/api/account").hasRole(Role.USER.name())
+                        .requestMatchers("/api/**").hasRole(Role.ADMIN.name())
+                )
+                .httpBasic(withDefaults());
+        return http.build();
     }
 }
