a7048bcd37d351db4653852b8436e95c513ba287
Александр Чистов <aleksandrchistov.ru@yandex.ru>
2024-09-07 20:01:34 +0700

    Error handling

---
+++ src/main/java/ru/aleksandrchistov/restaurantvoting/error/AppException.java
@@ -0,0 +1,18 @@
+package ru.aleksandrchistov.restaurantvoting.error;
+
+import lombok.Getter;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+@Getter
+public class AppException extends ResponseStatusException {
+    private final ErrorAttributeOptions options;
+    private final HttpStatus status;
+
+    public AppException(HttpStatus status, String message, ErrorAttributeOptions options) {
+        super(status, message);
+        this.options = options;
+        this.status = status;
+    }
+}
--- src/main/java/ru/aleksandrchistov/restaurantvoting/util/ValidationUtil.java
+++ src/main/java/ru/aleksandrchistov/restaurantvoting/util/ValidationUtil.java
@@ -1,12 +1,13 @@
 package ru.aleksandrchistov.restaurantvoting.util;
 
+import ru.aleksandrchistov.restaurantvoting.error.IllegalRequestDataException;
 import ru.aleksandrchistov.restaurantvoting.model.BaseEntity;
 
 public class ValidationUtil {
 
     public static void checkNew(BaseEntity entity) {
         if (!entity.isNew()) {
-            throw new IllegalArgumentException(entity + " must be new (id=null)");
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must be new (id=null)");
         }
     }
 
@@ -15,7 +16,7 @@ public class ValidationUtil {
         if (entity.isNew()) {
             entity.setId(id);
         } else if (entity.id() != id) {
-            throw new IllegalArgumentException(entity + " must has id=" + id);
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must has id=" + id);
         }
     }
 }
